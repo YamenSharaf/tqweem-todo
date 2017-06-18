6d419685fd4963dce17eccfc88e404d48972fbee
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-06-18 04:10:16 +0200

    more comments

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Core Vue.js
 import Vue from 'vue'
+// For material components
 import KeenUI from 'keen-ui'
+// For typing effect
 import VueTyperPlugin from 'vue-typer'
+// For toast messages
 import Toast from 'vue-easy-toast'
+// Main app code
 import App from './App'
+// Vue routing
 import router from './router'
 
 Vue.config.productionTip = false
 
+// Used plugins
 Vue.use(KeenUI)
 Vue.use(VueTyperPlugin)
 Vue.use(Toast)
